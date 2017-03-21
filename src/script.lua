6abc115adcd0339a2ea0de55989542a79b8b9221
Matteo Autieri <matteoautieri@live.it>
2017-03-21 11:04:54 +0100

    change and balaces

--- src/script.lua
+++ src/script.lua
@@ -4,7 +4,6 @@
 --
 -- @author  TyKonKet
 -- @date 22/12/2016
-
 DifficultiesImprove = {};
 DifficultiesImprove.name = "DifficultiesImprove";
 DifficultiesImprove.debug = false;
@@ -37,43 +36,40 @@ g_mpLoadingScreen.loadFunction = Utils.appendedFunction(g_mpLoadingScreen.loadFu
 
 function DifficultiesImprove:loadMap(name)
     self:print(("loadMap(name:%s)"):format(name));
-    if self.debug then
-        addConsoleCommand("AAADifficultiesImproveeTestCommand", "", "TestCommand", self);
-    end
     self:loadSavegame();
-    local difficullties = { EASY = 1, NORMAL = 2, HARD = 3 };     
+    local difficullties = {EASY = 1, NORMAL = 2, HARD = 3};
+    --EconomyManager.PRICE_MULTIPLIER = {2, 1.5, 1.0}
+    EconomyManager.PRICE_MULTIPLIER = {2.2, 1.5, 0.9}
+    --EconomyManager.COST_MULTIPLIER = {0.5, 0.75, 1.0}
+    EconomyManager.COST_MULTIPLIER = {0.4, 0.75, 1.1}
     if g_currentMission.missionInfo.difficulty == difficullties.EASY then
         self:print("Game difficulty is EASY!");
         --200.000
         g_currentMission.missionStats.loanMax = 2000000;
         --50
         g_currentMission.missionStats.loanAnnualInterestRate = 100;
-        --3
-        EconomyManager.MAX_GREAT_DEMANDS = 5;
-        --0.5
-        EconomyManager.COST_MULTIPLIER[1] = 0.4;
-        --2
-        EconomyManager.PRICE_MULTIPLIER[1] = 2.2;
         --0.01
         EconomyManager.PER_DAY_LEASING_FACTOR = 0.01;
-        --0.6
-        EconomyManager.PRICE_DROP_MIN_PERCENT = 0.4;
-        --1000
-        EconomyManager.CONFIG_CHANGE_PRICE = 500;
         --0.05
         EconomyManager.DEFAULT_RUNNING_LEASING_FACTOR = 0.02;
         --0.05
         EconomyManager.DEFAULT_LEASING_DEPOSIT_FACTOR = 0.01;
-        --1.2
-        EconomyManager.DIRECT_SELL_MULTIPLIER = 1.5;
+        --0.6
+        EconomyManager.PRICE_DROP_MIN_PERCENT = 0.4;
+        -- 0.08333
+        EconomyManager.LIFETIME_OPERATINGTIME_RATIO = 0.07333;
         --4
         EconomyManager.MAX_DAILYUPKEEP_MULTIPLIER = 3;
+        --1000
+        EconomyManager.CONFIG_CHANGE_PRICE = 500;
+        --1.2
+        EconomyManager.DIRECT_SELL_MULTIPLIER = 1.5;
         --60.000
         FieldDefinition.PRICE_PER_HA = 40000;
         --5000
         g_currentMission.husbandries.cow.animalDesc.price = 2000;
         --40
-	    g_currentMission.husbandries.cow.animalDesc.dailyUpkeep = 50;
+        g_currentMission.husbandries.cow.animalDesc.dailyUpkeep = 50;
         --0.02
         g_currentMission.husbandries.cow.animalDesc.birthRatePerDay = 0.1;
         --30
@@ -81,7 +77,7 @@ function DifficultiesImprove:loadMap(name)
         --3000
         g_currentMission.husbandries.pig.animalDesc.price = 500;
         --30
-	    g_currentMission.husbandries.pig.animalDesc.dailyUpkeep = 25;
+        g_currentMission.husbandries.pig.animalDesc.dailyUpkeep = 25;
         --0.16666666666667
         g_currentMission.husbandries.pig.animalDesc.birthRatePerDay = 0.2;
         --30
@@ -89,7 +85,7 @@ function DifficultiesImprove:loadMap(name)
         --4000
         g_currentMission.husbandries.sheep.animalDesc.price = 100;
         --20
-	    g_currentMission.husbandries.sheep.animalDesc.dailyUpkeep = 5;
+        g_currentMission.husbandries.sheep.animalDesc.dailyUpkeep = 5;
         --0.025
         g_currentMission.husbandries.sheep.animalDesc.birthRatePerDay = 0.15;
         --30
@@ -103,32 +99,28 @@ function DifficultiesImprove:loadMap(name)
         g_currentMission.missionStats.loanMax = 1000000;
         --100
         g_currentMission.missionStats.loanAnnualInterestRate = 200;
-        --3
-        EconomyManager.MAX_GREAT_DEMANDS = 3;
-        --0.75
-        EconomyManager.COST_MULTIPLIER[1] = 0.75;
-        --1.5
-        EconomyManager.PRICE_MULTIPLIER[1] = 1.5;
         --0.01
         EconomyManager.PER_DAY_LEASING_FACTOR = 0.02;
-        --0.6
-        EconomyManager.PRICE_DROP_MIN_PERCENT = 0.5;
-        --1000
-        EconomyManager.CONFIG_CHANGE_PRICE = 1500;
         --0.05
         EconomyManager.DEFAULT_RUNNING_LEASING_FACTOR = 0.04;
         --0.05
         EconomyManager.DEFAULT_LEASING_DEPOSIT_FACTOR = 0.035;
-        --1.2
-        EconomyManager.DIRECT_SELL_MULTIPLIER = 1.35;
+        --0.6
+        EconomyManager.PRICE_DROP_MIN_PERCENT = 0.5;
+        -- 0.08333
+        EconomyManager.LIFETIME_OPERATINGTIME_RATIO = 0.08333;
         --4
         EconomyManager.MAX_DAILYUPKEEP_MULTIPLIER = 5;
+        --1000
+        EconomyManager.CONFIG_CHANGE_PRICE = 1500;  
+        --1.2
+        EconomyManager.DIRECT_SELL_MULTIPLIER = 1.35;
         --60.000
         FieldDefinition.PRICE_PER_HA = 60000;
         --5000
         g_currentMission.husbandries.cow.animalDesc.price = 2500;
         --40
-	    g_currentMission.husbandries.cow.animalDesc.dailyUpkeep = 60;
+        g_currentMission.husbandries.cow.animalDesc.dailyUpkeep = 60;
         --0.02
         g_currentMission.husbandries.cow.animalDesc.birthRatePerDay = 0.08;
         --30
@@ -136,7 +128,7 @@ function DifficultiesImprove:loadMap(name)
         --3000
         g_currentMission.husbandries.pig.animalDesc.price = 650;
         --30
-	    g_currentMission.husbandries.pig.animalDesc.dailyUpkeep = 30;
+        g_currentMission.husbandries.pig.animalDesc.dailyUpkeep = 30;
         --0.16666666666667
         g_currentMission.husbandries.pig.animalDesc.birthRatePerDay = 0.18;
         --30
@@ -144,7 +136,7 @@ function DifficultiesImprove:loadMap(name)
         --4000
         g_currentMission.husbandries.sheep.animalDesc.price = 150;
         --20
-	    g_currentMission.husbandries.sheep.animalDesc.dailyUpkeep = 10;
+        g_currentMission.husbandries.sheep.animalDesc.dailyUpkeep = 10;
         --0.025
         g_currentMission.husbandries.sheep.animalDesc.birthRatePerDay = 0.12;
         --30
@@ -158,32 +150,28 @@ function DifficultiesImprove:loadMap(name)
         g_currentMission.missionStats.loanMax = 500000;
         --150
         g_currentMission.missionStats.loanAnnualInterestRate = 300;
-        --3
-        EconomyManager.MAX_GREAT_DEMANDS = 1;
-        --1
-        EconomyManager.COST_MULTIPLIER[1] = 1.1;
-        --1
-        EconomyManager.PRICE_MULTIPLIER[1] = 0.9;
         --0.01
         EconomyManager.PER_DAY_LEASING_FACTOR = 0.03;
-        --0.6
-        EconomyManager.PRICE_DROP_MIN_PERCENT = 0.6;
-        --1000
-        EconomyManager.CONFIG_CHANGE_PRICE = 2500;
         --0.05
         EconomyManager.DEFAULT_RUNNING_LEASING_FACTOR = 0.06;
         --0.05
         EconomyManager.DEFAULT_LEASING_DEPOSIT_FACTOR = 0.055;
-        --1.2
-        EconomyManager.DIRECT_SELL_MULTIPLIER = 1.15;
+        --0.6
+        EconomyManager.PRICE_DROP_MIN_PERCENT = 0.6;
+        -- 0.08333
+        EconomyManager.LIFETIME_OPERATINGTIME_RATIO = 0.09333;
         --4
         EconomyManager.MAX_DAILYUPKEEP_MULTIPLIER = 7;
+        --1000
+        EconomyManager.CONFIG_CHANGE_PRICE = 2500;
+        --1.2
+        EconomyManager.DIRECT_SELL_MULTIPLIER = 1.15;
         --60.000
         FieldDefinition.PRICE_PER_HA = 80000;
         --5000
         g_currentMission.husbandries.cow.animalDesc.price = 3000;
         --40
-	    g_currentMission.husbandries.cow.animalDesc.dailyUpkeep = 70;
+        g_currentMission.husbandries.cow.animalDesc.dailyUpkeep = 70;
         --0.02
         g_currentMission.husbandries.cow.animalDesc.birthRatePerDay = 0.04;
         --30
@@ -191,7 +179,7 @@ function DifficultiesImprove:loadMap(name)
         --3000
         g_currentMission.husbandries.pig.animalDesc.price = 800;
         --30
-	    g_currentMission.husbandries.pig.animalDesc.dailyUpkeep = 35;
+        g_currentMission.husbandries.pig.animalDesc.dailyUpkeep = 35;
         --0.16666666666667
         g_currentMission.husbandries.pig.animalDesc.birthRatePerDay = 0.15;
         --30
@@ -199,7 +187,7 @@ function DifficultiesImprove:loadMap(name)
         --4000
         g_currentMission.husbandries.sheep.animalDesc.price = 200;
         --20
-	    g_currentMission.husbandries.sheep.animalDesc.dailyUpkeep = 15;
+        g_currentMission.husbandries.sheep.animalDesc.dailyUpkeep = 15;
         --0.025
         g_currentMission.husbandries.sheep.animalDesc.birthRatePerDay = 0.1;
         --30
@@ -230,10 +218,6 @@ end
 
 function DifficultiesImprove:deleteMap()
     self:print("deleteMap()");
-end
-
-function DifficultiesImprove:TestCommand()
-    return "This is a test command";
 end
 
 function DifficultiesImprove:keyEvent(unicode, sym, modifier, isDown)
